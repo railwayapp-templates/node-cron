932f72f1a589f2057f0d5960abc86dc06bd191df
Faraz Patankar <farazpatankar@gmail.com>
2022-08-18 13:45:45 +0400

    change log message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import cron from 'node-cron';
 
 cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`${new Date().toLocaleString()}: running your task...`);
+  console.log(`running your task...`);
 });
